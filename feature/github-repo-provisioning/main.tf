31f6681463ed48bc69b3b887e7d9e57afb487611
Ivan Pavlovic <ivan@gr-oss.io>
2025-10-13 16:35:05 +0200

    wip

--- feature/github-repo-provisioning/main.tf
+++ feature/github-repo-provisioning/main.tf
@@ -1,11 +1,11 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "6.6.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     github = {
+#       source  = "integrations/github"
+#       version = "6.6.0"
+#     }
+#   }
+# }
 provider "github" {
   owner = var.owner
   app_auth {
