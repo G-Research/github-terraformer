8ebaf157538de7b2e7154d24ead319fed78e0c68
Ivan Pavlovic <ivan@gr-oss.io>
2026-05-06 11:47:33 +0200

    Test

--- feature/github-repo-provisioning/modules/terraform-github-repository/main.tf
+++ feature/github-repo-provisioning/modules/terraform-github-repository/main.tf
@@ -192,7 +192,6 @@ resource "github_branch_default" "default" {
 
   repository = github_repository.repository.name
   branch     = local.default_branch
-  rename     = true
 
   depends_on = [github_branch.branch]
 }
